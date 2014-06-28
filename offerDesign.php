47f99d1185529739dbd7b36eacc8d95bb7755529
Stoyanov <stoyanov.vn@gmail.com>
2014-06-29 00:09:53 +0300

    Rearranged filenames.

---
+++ offerDesign.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>JokeUni</title>
+		<meta charset="utf8"/>
+		<link href="styles/JokeUniMainStyle.css" rel="stylesheet" type="text/css">
+			
+	</head>
+
+	<body>
+		<header>
+			<?php include ('header.php'); ?>
+		</header>
+		<nav>
+			<ul>
+				<li><a href="index.php" class="navigation">Начало</a></li>
+				<li><a href="quotes.php" class="navigation">Цитати</a></li>
+				<li><a href="jokes.php" class="navigation">Бисери</a></li>
+			</ul>
+		</nav>
+		
+		<section>
+			Some Text
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+		</section>
+		
+		<aside>
+			<h2>Вицове за програмисти:</h2>
+			<ul class="block">
+				<li><a href="http://softuni.bg/forum/questions/details/38" target="_blank">SoftUniForum</a></li>
+				<li><a href="http://animacii.net/humor/1624-zabavni-programisti-programirane-smeshni-smqh-humor.html" target="_blank">101 начина да вбесите програмиста!</a></li>
+				<li><a href="http://www.vicmania.com/?cat=1059&amp;paged=4" target="_blank">Виц Мания</a></li>
+				<li><a href="http://www.yes.bg/fun/?cat=24" target="_blank"> Още вицове</a></li>
+			</ul>
+			<img src="Images/LaughingFace.jpg" alt="LaughingFace" class="laughingFace">
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+		</aside>
+		
+		<footer>
+            <?php include ('footer.php'); ?>
+        </footer>
+	</body>
+</html>
