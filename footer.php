3dd64f787eda58e888298534affd616a15ed1a59
Stoyanov <stoyanov.vn@gmail.com>
2014-06-30 10:42:23 +0300

    Header and footer improvements.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<strong>&copy; June, 2014 - TEAM "ENYWAS":</strong>
+<!-- <strong>&copy; June, 2014 - TEAM "ENYWAS":</strong> -->
 <!--should be redesigned as divs-->
-<table>
+<!-- <table>
     <tr>
         <td>Stoyan Stoyanov</td>
         <td>Mihaela Mladenova</td>
@@ -11,4 +11,19 @@
         <td>Nikolay Rangelov</td>
         <td>Yulia Dimitrova-Jamieson</td>
     </tr>
-</table>
+</table> -->
+<div class="footWrapper">
+    <div><strong>&copy; June, 2014 - TEAM "ENYWAS":</strong></div>
+    <div>
+        Stoyan Stoyanov
+        Mihaela Mladenova
+    </div>
+    <div>
+        Teodor Dobrev
+        Svetoslav Toshkov
+    </div>
+    <div>
+        Nikolay Rangelov
+        Yulia Dimitrova-Jamieson
+    </div>
+</div>
