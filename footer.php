e37503211b56b9ce0195edeb28998b137c4516a7
yuletodim <yuletodim@users.noreply.github.com>
2014-06-30 12:58:07 +0300

    add valid HTML button + icon

--- footer.php
+++ footer.php
@@ -12,9 +12,16 @@
         Nikolay Rangelov
         Yulia Dimitrova-Jamieson
     </div>
-    <div>
-        <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fstoyanstoyanov.eu%2Fjokesite%2F">
-            <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Валиден CSS!"/>
-        </a>
-    </div>
+	<div>
+		<div>
+			<a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fstoyanstoyanov.eu%2Fjokesite%2F" target="_blank">
+				<img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Валиден CSS!"/>
+			</a>
+		</div>
+		<div>
+			<a href="http://validator.w3.org/check?uri=http%3A%2F%2Fstoyanstoyanov.eu%2Fjokesite%2F" target="_blank">
+				<img src="Images/validHTML.jpg" style="border:0;width:88px;height:31px"  alt="Валиден HTML!"/>
+			</a>
+		</div>
+	</div>
 </div>
