af9263dff384bfb56bd65aca2cda6c318cf6726d
Stoyanov <stoyanov.vn@gmail.com>
2014-06-30 12:19:55 +0300

    Added CSS validated image in the footer.

--- footer.php
+++ footer.php
@@ -12,4 +12,9 @@
         Nikolay Rangelov
         Yulia Dimitrova-Jamieson
     </div>
+    <div>
+        <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fstoyanstoyanov.eu%2Fjokesite%2F">
+            <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Валиден CSS!"/>
+        </a>
+    </div>
 </div>
